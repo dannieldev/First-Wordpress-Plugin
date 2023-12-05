6505d847d4aa35da6bf7178197fbb5b81345c84c
dannieldev <danielcrespin.dev.com>
2023-12-05 13:44:57 -0600

    Insertar codigo en el header de wordpress

--- index.php
+++ index.php
@@ -8,3 +8,11 @@ Author: dannieldev
 
 // Código del plugin aquí
 
+add_action( 'wp_head', 'agregar_ga' );
+function agregar_ga() {
+    echo "
+    <script src='https://code.jquery.com/jquery-3.6.4.min.js'></script>
+    <script src='https://cdnjs.cloudflare.com/ajax/libs/jqueryui/1.11.2/jquery-ui.min.js'></script>
+    <script src='https://cdnjs.cloudflare.com/ajax/libs/handlebars.js/3.0.0/handlebars.min.js'></script>
+    <script src='https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js'></script>";
+}
